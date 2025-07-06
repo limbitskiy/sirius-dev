8ec65257c1bce899fd58c688ef97dcc71b275704
limbitskiy86 <gagarinbrood@gmail.com>
2025-07-06 20:14:17 +0300

    remove unused store  functions

--- middleware/setTimezone.global.ts
+++ middleware/setTimezone.global.ts
@@ -3,15 +3,21 @@ import { useUserStore } from "~/store/user";
 export default defineNuxtRouteMiddleware(async (_to, _from) => {
   const cookieTimezone = useCookie("timezone").value;
   const nuxtApp = useNuxtApp();
+
   if (!cookieTimezone) return;
+
   const userStore = useUserStore();
+
   const { setLoading } = userStore;
+
   const { data: timezones } = await useFetch("/api/timezones");
+
   if (timezones.value!.includes(cookieTimezone)) {
     await nuxtApp.$i18n.setLocale("ru");
   } else {
     await nuxtApp.$i18n.setLocale("en");
   }
+
   nuxtApp.hook("page:finish", () => {
     setLoading(false);
     document.body.style.overflow = "auto";
--- store/user.ts
+++ store/user.ts
@@ -2,49 +2,17 @@ import { defineStore } from "pinia";
 
 export const useUserStore = defineStore("user", () => {
   const user = ref({
-    locale: "ru",
     loading: true,
-    modalOpen: false,
-    notificationOpen: false,
-    menuOpen: false,
   });
 
-  const locale = computed(() => user.value.locale);
   const loading = computed(() => user.value.loading);
-  const modalOpen = computed(() => user.value.modalOpen);
-  const notificationOpen = computed(() => user.value.notificationOpen);
-  const menuOpen = computed(() => user.value.menuOpen);
-
-  const setLocale = (locale: string) => {
-    user.value.locale = locale;
-  };
 
   const setLoading = (value: boolean) => {
     user.value.loading = value;
   };
 
-  const setModalOpen = (value: boolean) => {
-    user.value.modalOpen = value;
-  };
-
-  const setNotificationOpen = (value: boolean) => {
-    user.value.notificationOpen = value;
-  };
-
-  const setMenuOpen = (value: boolean) => {
-    user.value.menuOpen = value;
-  };
-
   return {
-    locale,
     loading,
-    modalOpen,
-    notificationOpen,
-    menuOpen,
-    setLocale,
     setLoading,
-    setModalOpen,
-    setNotificationOpen,
-    setMenuOpen,
   };
 });
